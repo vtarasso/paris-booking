9f52f158846de2784931e7e5b478f0109992a707
Vitaliy Tarassov <apxustudio@gmail.com>
2023-08-24 15:51:53 +0600

    add: app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const burger = document.querySelector('.header__burger')
+const menu = document.querySelector('.header__nav')
+const close = document.querySelector('.header__nav-close')
+const menuElements = document.querySelectorAll('.header__link')
+
+for (let i = 0; i < menuElements.length; i++) {
+    menuElements[i].addEventListener('click', function(e){
+        e.preventDefault()
+        let href = this.getAttribute('href')
+
+        let currentSection = document.getElementById(href)
+
+        let offset = currentSection.offsetTop
+
+        menu.classList.remove('header__nav-open')
+        
+        window.scrollTo(document.body, offset, 0);
+    })
+}
+
+burger.addEventListener('click', function(e){
+    e.preventDefault()
+    menu.classList.add('header__nav-open')
+})
+
+close.addEventListener('click', function(e){
+    e.preventDefault()
+    menu.classList.remove('header__nav-open')
+})
+
